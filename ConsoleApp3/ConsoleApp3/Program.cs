34700fc272a178835a37b0cf717d7c8e39a0a0c4
Bratu Alexandru <alexandru.bratu@outlook.com>
2017-08-10 10:29:11 +0300

    Commit 6

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -15,19 +15,19 @@ namespace ConsoleApp3
             var container = new WindsorContainer();
 
             container.Register(Component.For<ILogin>().ImplementedBy<Login>());
-            container.Register(Component.For<IBook>().ImplementedBy<Book>().Named("Book1").DependsOn(new { name = "Book1", author = "Author1", type = "Type1", code = 1, genre = "Genre1" }));
-            container.Register(Component.For<IBook>().ImplementedBy<Book>().Named("Book2").DependsOn(new { name = "Book2", author = "Author2", type = "Type2", code = 2, genre = "Genre2" }));
-            container.Register(Component.For<IBook>().ImplementedBy<Book>().Named("Book3").DependsOn(new { name = "Book3", author = "Author3", type = "Type3", code = 3, genre = "Genre3" }));
+            container.Register(Component.For<IBook>().ImplementedBy<Book>().LifestyleTransient());
+            
 
             var librarian = Librarian.Instance("Librarian", "Admin", "pass");
 
             var library = Library.Instance("Library", librarian);
 
-            library.AddBook(container.Resolve<IBook>("Book2"));
-            library.AddBook(container.Resolve<IBook>("Book3"));
-            library.AddBook(container.Resolve<IBook>("Book1"));
+            library.AddBook(container.Resolve<IBook>(new { name = "Book2", author = "Author2", type = "Type2", code = 2, genre = "Genre2" }));
+            library.AddBook(container.Resolve<IBook>(new { name = "Book3", author = "Author3", type = "Type3", code = 3, genre = "Genre3" }));
+            library.AddBook(container.Resolve<IBook>(new { name = "Book1", author = "Author1", type = "Type1", code = 1, genre = "Genre1" }));
 
-            
+
+
 
             var login = container.Resolve<ILogin>(new { librarian = librarian, library = library });
 
